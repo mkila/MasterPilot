094dcc623f4576a8adc4469aca6affadddbc6423
Sybille <sybille@sybille-Inspiron-1545.(none)>
2013-12-08 17:59:50 +0100

    Moteur Physique

--- src/fr/umlv/physics/PhysicsEngine.java
+++ src/fr/umlv/physics/PhysicsEngine.java
@@ -1,13 +1,13 @@
 package fr.umlv.physics;
 
 import org.jbox2d.common.Vec2;
-import org.jbox2d.dynamics.BodyDef;
 import org.jbox2d.dynamics.World;
 
 public class PhysicsEngine {
-	private final World world = new World(new Vec2(0, 0));
+	private static final World world = new World(new Vec2(0, 0));
 	
-	public void createBodys(BodyDef def){
-		world.createBody(def);
+	public static World getWorld(){
+		return world;
 	}
+	
 }
