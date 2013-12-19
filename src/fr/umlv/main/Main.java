462d094fcd1cba12e963075fbd864863353aea40
Duke12 <duke@ubuntu.(none)>
2013-12-19 15:31:07 +0100

    modify movement.

--- src/fr/umlv/main/Main.java
+++ src/fr/umlv/main/Main.java
@@ -33,6 +33,7 @@ public class Main {
 		SpaceObject planet2 = new Planet(new ServicePlanet(PhysicsEngine.getWorld(), 100, new Vec2(800,600)));
 		//			    hero.getService().getBody().setLinearVelocity(new Vec2(hero.getService().getBody().getPosition().x,
 		//			    		hero.getService().getBody().getPosition().y--));
+		
 
 		System.out.println(hero.getService().getBody().getPosition().toString());
 		Application.run("MasterPilot", WIDTH, HEIGHT, context -> {
@@ -56,13 +57,13 @@ public class Main {
 						return;
 					}
 					if(event.getKey() == KeyboardKey.DOWN){
-						Vec2 tmp = new Vec2(hero.getService().getBody().getWorldCenter().x,hero.getService().getBody().getWorldCenter().y--);
+						Vec2 tmp = new Vec2(0,hero.getService().getBody().getWorldCenter().y--);
 						hero.getService().getBody().applyLinearImpulse(tmp.mul(1), hero.getService().getBody().getWorldCenter());
 						//GraphicsEngine.drawSpaceObject(context,hero);
 
 					}
 					if(event.getKey() == KeyboardKey.UP){
-						Vec2 tmp = new Vec2(hero.getService().getBody().getWorldCenter().x,hero.getService().getBody().getWorldCenter().y++);
+						Vec2 tmp = new Vec2(0,hero.getService().getBody().getWorldCenter().y++);
 						hero.getService().getBody().applyLinearImpulse(tmp.mul(1), hero.getService().getBody().getWorldCenter());
 						//GraphicsEngine.drawSpaceObject(context,hero);
 
@@ -73,7 +74,9 @@ public class Main {
 						//hero.getService().getBody().applyLinearImpulse(tmp.mul(10), hero.getService().getBody().getPosition());
 						float desiredAngle = (float) atan2( -tmp.x, tmp.y );
 						float totalRotation = desiredAngle - bodyAngle;
-						float change = (float) (1 * Math.toRadians(totalRotation)); 
+						while ( totalRotation < Math.toRadians(-180) ) totalRotation += Math.toRadians(360);
+						while ( totalRotation > Math.toRadians(180) ) totalRotation -= Math.toRadians(360);
+						float change = (float) (Math.toRadians(5)); 
 						float newAngle = bodyAngle + Math.min( change, Math.max(-change, totalRotation));
 						hero.getService().getBody().setTransform( hero.getService().getBody().getPosition(), newAngle );
 						//GraphicsEngine.drawSpaceObject(context,hero);
@@ -87,7 +90,9 @@ public class Main {
 						//hero.getService().getBody().applyLinearImpulse(tmp.mul(100), hero.getService().getBody().getPosition());
 						float desiredAngle = (float) atan2( -tmp.x, tmp.y );
 						float totalRotation = desiredAngle - bodyAngle;
-						float change = (float) (1 * Math.toRadians(totalRotation)); 
+						while ( totalRotation < Math.toRadians(-180) ) totalRotation += Math.toRadians(360);
+						while ( totalRotation > Math.toRadians(180) ) totalRotation -= Math.toRadians(360);
+						float change = (float) (Math.toRadians(5)); 
 						float newAngle = bodyAngle + Math.min( change, Math.max(-change, totalRotation));
 						hero.getService().getBody().setTransform( hero.getService().getBody().getPosition(), newAngle );
 						//GraphicsEngine.drawSpaceObject(context,hero);
