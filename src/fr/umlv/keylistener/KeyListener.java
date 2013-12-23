405bfbb775de4d1ce16106a61b757c34f528e616
Sybille <sybille@sybille-Inspiron-1545.(none)>
2013-12-23 14:00:06 +0100

    finalisation des déplacements et deplacement des méthodes de Keyevent

--- src/fr/umlv/keylistener/KeyListener.java
+++ src/fr/umlv/keylistener/KeyListener.java
@@ -1,83 +1,47 @@
 package fr.umlv.keylistener;
 
-import java.util.ArrayList;
 
+import org.jbox2d.common.Vec2;
+import fr.umlv.space.object.SpaceObject;
 import fr.umlv.zen3.ApplicationContext;
 import fr.umlv.zen3.KeyboardEvent;
 import fr.umlv.zen3.KeyboardKey;
 
 public class KeyListener {
 
-	private ArrayList<KeyboardEvent> list;
-	public enum State {UNKNOW,UP,DOWN,LEFT,RIGHT};
-	private State state;
 
-	public KeyListener(){
-		list = new ArrayList<>();
-		state = State.UNKNOW;
-	}
+	public static void listen(SpaceObject hero,ApplicationContext context) {
+		context.render(graphics -> {
+			KeyboardEvent event = context.pollKeyboard();
+			if (event == null) {
+				return;
+			}
+			if(event.getKey() == KeyboardKey.DOWN){
+//				Vec2 tmp = new Vec2(0,hero.getService().getBody().getWorldCenter().y);
+//				hero.getService().getBody().applyForceToCenter(tmp.mul(100));
 
-	public void movement(ApplicationContext context) {
-		KeyboardEvent ke = null;
-		try {
-			ke = context.pollKeyboard();
-		} catch(NullPointerException o) {
-		}
-		if(ke == null){
-			return;
-		}
+			}
+			if(event.getKey() == KeyboardKey.UP){
+				Vec2 tmp = new Vec2((-(float)Math.sin(hero.getService().getBody().getAngle()))*1000,
+									((float)Math.cos(hero.getService().getBody().getAngle()))*1000);
+				hero.getService().getBody().applyForceToCenter(tmp.mul(100));
 
-		if(ke.getKey() == KeyboardKey.LEFT){
-			list.add(ke);
-			state = State.LEFT;
-			//x--
-			return;
-		}
+			}
+			if(event.getKey() == KeyboardKey.RIGHT){
+				float time = 1; // one second
+				float torque = hero.getService().getBody().getInertia() * (1 - hero.getService().getBody().getAngularVelocity() ) / time;
+				hero.getService().getBody().applyTorque(torque);
+				System.out.println("angle :"+hero.getService().getBody().getAngle());
 
-		if(ke.getKey() == KeyboardKey.RIGHT){
-			list.add(ke);
-			state = State.RIGHT;
-			//x++
-			return;
-		}
-		
-		if(ke.getKey() == KeyboardKey.UP){
-			list.add(ke);
-			state = State.UP;
-			//y++
-			return;
-		}
-		
-		if(ke.getKey() == KeyboardKey.DOWN){
-			list.add(ke);
-			state = State.DOWN;
-			//y--
-			return;
-		}
-	}
-	
-	/**
-	 * Set the state of the movement 
-	 * 
-	 */
-	public void setState(State state) {
-		this.state = state;
-	}
-	
-	/**
-	 * Get the list of KeyboardEvent
-	 * @return the list
-	 */
-	public ArrayList<KeyboardEvent> getList() {
-		return list;
-	}
-	
-	/**
-	 * Get the state of the movement 
-	 * @return the state 
-	 */
-	public State getState() {
-		return state;
-	}
+			}
+			if(event.getKey() == KeyboardKey.LEFT){
+				float time = 1; // one second
+				float torque = hero.getService().getBody().getInertia() * (-1 - hero.getService().getBody().getAngularVelocity() ) / time;
+				hero.getService().getBody().applyTorque(torque);
+				System.out.println("angle :"+hero.getService().getBody().getAngle());
 
+			}
+
+		});
+	}
 }
