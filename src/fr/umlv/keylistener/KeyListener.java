c2cdc9247df094362b4f03c5da9f46ab36056f4d
Sybille <sybille@sybille-Inspiron-1545.(none)>
2013-12-24 13:09:26 +0100

    ajout du case de l'évenement SPACE

--- src/fr/umlv/keylistener/KeyListener.java
+++ src/fr/umlv/keylistener/KeyListener.java
@@ -3,10 +3,7 @@ package fr.umlv.keylistener;
 
 import org.jbox2d.common.Vec2;
 
-import fr.umlv.physics.PhysicsEngine;
-import fr.umlv.space.object.Bullet;
 import fr.umlv.space.object.SpaceObject;
-import fr.umlv.space.service.ServiceBullet;
 import fr.umlv.zen3.ApplicationContext;
 import fr.umlv.zen3.KeyboardEvent;
 import fr.umlv.zen3.KeyboardKey;
@@ -14,7 +11,7 @@ import fr.umlv.zen3.KeyboardKey;
 public class KeyListener {
 
 
-	public static void listen(SpaceObject hero, SpaceObject bullet, ApplicationContext context) {
+	public static void listen(SpaceObject hero, ApplicationContext context) {
 		context.render(graphics -> {
 			KeyboardEvent event = context.pollKeyboard();
 			if (event == null) {
@@ -23,13 +20,13 @@ public class KeyListener {
 			if(event.getKey() == KeyboardKey.DOWN){
 				Vec2 tmp = new Vec2((-(float)Math.sin(hero.getService().getBody().getAngle()))*1000,
 						((float)Math.cos(hero.getService().getBody().getAngle()))*1000);
-	hero.getService().getBody().applyForceToCenter(tmp.mul(-1000));
+	hero.getService().getBody().applyForceToCenter(tmp.mul(-100));
 
 			}
 			if(event.getKey() == KeyboardKey.UP){
 				Vec2 tmp = new Vec2((-(float)Math.sin(hero.getService().getBody().getAngle()))*1000,
 									((float)Math.cos(hero.getService().getBody().getAngle()))*1000);
-				hero.getService().getBody().applyForceToCenter(tmp.mul(1000));
+				hero.getService().getBody().applyForceToCenter(tmp.mul(100));
 
 			}
 			if(event.getKey() == KeyboardKey.RIGHT){
@@ -48,10 +45,7 @@ public class KeyListener {
 			}
 			
 			if(event.getKey() == KeyboardKey.SPACE){
-				bullet.getService().getBody().setLinearVelocity(new Vec2((float)(10 * Math.cos(hero.getService().getBody().getAngle())),
-						(float)(10 * Math.sin(hero.getService().getBody().getAngle()))));
-				System.out.println("bullet :"+bullet.getService().getBody().getPosition().toString());
-				
+				hero.getService().fire();
 			}
 
 		});
