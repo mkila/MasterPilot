8d7da63c4efc28fc96e350c14eb13ef0a3a444bb
Duke12 <duke@ubuntu.(none)>
2013-12-25 16:28:29 +0100

    Add a timer in second.

--- src/fr/umlv/main/Game.java
+++ src/fr/umlv/main/Game.java
@@ -7,7 +7,15 @@ import java.awt.Color;
 
 
 
+
+import java.awt.Font;
+
+import org.jbox2d.common.Timer;
 import org.jbox2d.common.Vec2;
+
+
+
+
 
 
 
@@ -32,6 +40,7 @@ public class Game implements GameManager{
 		float timeStep = 1.0f/60.0f;      //the length of time passed to simulate (seconds)
 		int velocityIterations = 2;   //how strongly to correct velocity
 		int positionIterations = 2; 
+		Timer t = new Timer();
 		SpaceObject hero = new SpaceShip(new ServiceHero(PhysicsEngine.getWorld()));
 		SpaceObject planet1 = new Planet(new ServicePlanet(PhysicsEngine.getWorld(), 100, new Vec2(0,0)));
 		SpaceObject planet2 = new Planet(new ServicePlanet(PhysicsEngine.getWorld(), 100, new Vec2(800,600)));
@@ -49,6 +58,12 @@ public class Game implements GameManager{
 			GraphicsEngine.drawSpaceObject(context,planet2,hero.getService().getBody().getWorldCenter());
 			GraphicsEngine.drawFire(context, hero,hero.getService().getBody().getWorldCenter());
 			KeyListener.listen(hero, context);
+			context.render(graphics -> {
+					graphics.setColor(Color.WHITE);
+					graphics.setFont(new Font("Courrier", Font.BOLD, 20));
+					graphics.drawString(String.valueOf(String.format("%1.0f",t.getMilliseconds()/1000)), 750,20);
+			});
+
 
 		}
 
