e8af579aab73561dc91ce98aa285d5c0ec317570
Sybille <sybille@sybille-Inspiron-1545.(none)>
2013-12-27 17:23:00 +0100

    Service Enemis de base!

---
+++ src/fr/umlv/space/service/ServiceArmada.java
@@ -0,0 +1,13 @@
+package fr.umlv.space.service;
+
+import org.jbox2d.dynamics.Body;
+
+public class ServiceArmada implements Service{
+
+	@Override
+	public Body getBody() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
