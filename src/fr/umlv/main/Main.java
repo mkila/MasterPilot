0f24eaf6dc004d5d45baa8fe93ad88062b127b32
Sybille <sybille@sybille-Inspiron-1545.(none)>
2013-12-27 17:25:46 +0100

    Main de test

--- src/fr/umlv/main/Main.java
+++ src/fr/umlv/main/Main.java
@@ -6,7 +6,9 @@ import java.awt.Color;
 
 
 
+
 import org.jbox2d.common.Vec2;
+
 
 
 
@@ -20,6 +22,7 @@ import fr.umlv.space.object.SpaceObject;
 import fr.umlv.space.object.SpaceShip;
 import fr.umlv.space.service.ServiceHero;
 import fr.umlv.space.service.ServicePlanet;
+import fr.umlv.space.service.ServiceTIE;
 import fr.umlv.zen3.Application;
 
 public class Main {
@@ -35,7 +38,7 @@ public class Main {
 		SpaceObject hero = new SpaceShip(new ServiceHero(PhysicsEngine.getWorld()));
 		SpaceObject planet1 = new Planet(new ServicePlanet(PhysicsEngine.getWorld(), 100, new Vec2(0,0)));
 		SpaceObject planet2 = new Planet(new ServicePlanet(PhysicsEngine.getWorld(), 100, new Vec2(800,600)));
-		
+		SpaceObject enemiTIE = new SpaceShip(new ServiceTIE(PhysicsEngine.getWorld(),new Vec2(300,60)));
 		System.out.println(hero.getService().getBody().getPosition().toString());
 		Application.run("MasterPilot", WIDTH, HEIGHT, context -> {
 			for(;;) {
@@ -44,15 +47,20 @@ public class Main {
 				} catch (InterruptedException e) {
 					Thread.currentThread().interrupt();
 				}
+				enemiTIE.getService().fire(hero.getService().getBody().getWorldCenter());
 				PhysicsEngine.getWorld().step(timeStep,velocityIterations,positionIterations);
 				GraphicsEngine.graphicClear(context);
 				GraphicsEngine.setBackground(context,Color.BLACK);
 				GraphicsEngine.drawSpaceObject(context,hero,hero.getService().getBody().getWorldCenter());
 				GraphicsEngine.drawSpaceObject(context,planet1,hero.getService().getBody().getWorldCenter());
 				GraphicsEngine.drawSpaceObject(context,planet2,hero.getService().getBody().getWorldCenter());
+				//GraphicsEngine.drawSpaceObject(context,enemiTIE,hero.getService().getBody().getWorldCenter());
+				GraphicsEngine.drawTIE(context, enemiTIE, hero.getService().getBody().getWorldCenter());
 				GraphicsEngine.drawFire(context, hero,hero.getService().getBody().getWorldCenter());
+				GraphicsEngine.drawFire(context, enemiTIE,hero.getService().getBody().getWorldCenter());
 				KeyListener.listen(hero, context);
-				
+				PhysicsEngine.tieBehavior(enemiTIE,hero.getService().getBody().getWorldCenter());
+
 			}
 		});
 	}
