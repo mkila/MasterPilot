e0ba08af4308ec5db63aaf134aa65e4184a67aed
Sybille <sybille@sybille-Inspiron-1545.(none)>
2013-12-29 10:41:58 +0100

    Contacte liustener! traite les collisions

--- src/fr/umlv/physics/PhysicsEngine.java
+++ src/fr/umlv/physics/PhysicsEngine.java
@@ -1,17 +1,51 @@
 package fr.umlv.physics;
 
+import org.jbox2d.callbacks.ContactImpulse;
+import org.jbox2d.callbacks.ContactListener;
+import org.jbox2d.collision.Manifold;
 import org.jbox2d.common.Vec2;
 import org.jbox2d.dynamics.World;
+import org.jbox2d.dynamics.contacts.Contact;
+
 import fr.umlv.space.object.SpaceObject;
+import fr.umlv.space.service.Service;
 
 public class PhysicsEngine {
 	private static final World world = new World(new Vec2(0, 0));
-
+	
 	public static World getWorld() {
+		ContactListener contactListener = new ContactListener() {
+			
+			@Override
+			public void preSolve(Contact arg0, Manifold arg1) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void postSolve(Contact arg0, ContactImpulse arg1) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void endContact(Contact arg0) {
+				
+			}
+			
+			@Override
+			public void beginContact(Contact arg0) {
+				((Service)arg0.getFixtureA().getUserData()).collision();
+				((Service)arg0.getFixtureB().getUserData()).collision();
+			}
+		};
+		world.setContactListener(contactListener);
 		return world;
 	}
 
 	public static void tieBehavior(SpaceObject tie, Vec2 positionHero) {
+		if(tie.getService().getBody()==null)
+			return;
 		Vec2 tmp = new Vec2((-(float) Math.sin(tie.getService().getBody()
 				.getAngle())) * 1000, ((float) Math.cos(tie.getService()
 				.getBody().getAngle())) * 1000);
